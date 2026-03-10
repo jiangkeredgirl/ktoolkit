b6754c3e25c2ff00bd806a2a38bb2e216e972eb0
jiangke <jiangkeredgirl@hotmail.com>
2026-03-10 11:17:06 +0800

    从KlogSDK重命名KLogSDK

---
+++ KLogSDK/include/klogcustomsetting.h
@@ -0,0 +1,50 @@
+﻿#pragma once 
+
+
+// 是否生成trace 函数
+#define TRACE_OUT             1                     ///< 0 not output log, 1 output log
+#define TRACK_OUT             1                     ///< whether output trace of function called
+// 是否使用glog
+#define USE_GLOG              0
+
+// 预定义8种trace等级，数字越大输出等级越低
+#define TRACE_TRACk           1                     ///< track out,0 not output
+#define TRACE_ERROR           2                     ///< error out,0 not output
+#define TRACE_WARNING         3                     ///< warning out,0 not output
+#define TRACE_OK              4                     ///< ok out,0 not output
+#define TRACE_NOTICE          5                     ///< notice out,0 not output
+#define TRACE_INFO            6                     ///< info out,0 not output
+#define TRACE_DEBUG           7                     ///< debug out,0 not output
+#define TRACE_TEMP            8                     ///< temporary out,0 not output
+
+// log 标签
+#define TRACE_LABEL          ("kk")                 ///< my output label
+
+// 定义一些常用的trace宏
+// c风格char类型 编译期可以去掉trace代码
+#define KlogError(log_format, ...)          Trace(TRACE_ERROR,          log_format, ##__VA_ARGS__)
+#define KlogWarn(log_format, ...)           Trace(TRACE_WARNING,        log_format, ##__VA_ARGS__)
+#define KlogOK(log_format, ...)             Trace(TRACE_OK,             log_format, ##__VA_ARGS__)
+#define KlogNotice(log_format, ...)         Trace(TRACE_NOTICE,         log_format, ##__VA_ARGS__)
+#define KlogInfo(log_format, ...)           Trace(TRACE_INFO,           log_format, ##__VA_ARGS__)
+#define KlogDebug(log_format, ...)          Trace(TRACE_DEBUG,          log_format, ##__VA_ARGS__)
+#define KlogTemp(log_format, ...)           Trace(TRACE_TEMP,           log_format, ##__VA_ARGS__)
+
+// c++风格char类型 编译期不可以去掉trace代码
+#define KlogErrorCout                       TraceCout(TRACE_ERROR     )
+#define KlogWarnCout                        TraceCout(TRACE_WARNING   )
+#define KlogOKCout                          TraceCout(TRACE_OK        )
+#define KlogNoticeCout                      TraceCout(TRACE_NOTICE    )
+#define KlogInfoCout                        TraceCout(TRACE_INFO      )
+#define KlogDebugCout                       TraceCout(TRACE_DEBUG     )
+#define KlogTempCout                        TraceCout(TRACE_TEMP      )
+
+//c 风格char类型 函数进入退出trace
+#define KlogTrack(log_format, ...)          TraceTrack(TRACE_TRACk,  log_format, ##__VA_ARGS__)
+//c++ 风格char类型 函数进入退出trace
+#define KlogTrackCout                       TraceTrackCout(TRACE_TRACk)
+
+#define KlogWaitAsyncEnd                    TraceWaitAsyncEnd
+#define KlogCreateStdout                    TraceCreateStdout
+#define KlogSetStdoutUTF8                   TraceSetStdoutUTF8
+#define KlogGenerateDump                    TraceGenerateDump
---
+++ KLogSDK/include/traceprinter.h
@@ -0,0 +1,129 @@
+﻿#pragma once
+
+#ifdef KLOGLIB_EXPORTS
+#define KLOGLIB_API __declspec(dllexport)
+#else
+#define KLOGLIB_API __declspec(dllimport)
+#endif
+
+#include <string>
+#include <map>
+#include <Windows.h>
+using namespace std;
+
+namespace kk
+{
+	struct TraceConfig
+	{
+		bool trace_out;
+		bool track_out;
+		bool output_compile;
+		bool output_console;
+		bool output_file;
+		bool output_socket;
+		bool output_com;
+		bool file_level;  ///< 是否分等级显示
+		bool file_module; ///< 是否分模块显示
+		bool file_date;   ///< 是否分日期显示
+		bool async;
+		bool head;
+		bool head_index;
+		bool head_functrack;
+		bool head_functime;
+		bool head_level;
+		bool head_label;
+		bool head_threadid;
+		bool head_processname;
+		bool head_modulename;
+		bool head_filename;
+		bool head_funcname;
+		bool head_line;
+		bool head_datetime;
+		bool head_runtime;
+		bool head_async;
+		int valid_level;	      // 有效输出等级
+		int valid_console_level;  // 控制台有效输出等级
+		int console_format;       // 默认 0 json模式， 1 print或者cout模式
+		int trace_file_size;
+		string head_label_text;
+		string trace_file_dir;
+		string trace_com_name;
+		map<int/*level*/, bool> level_on_off;
+		string trace_server_ip;
+		int trace_server_control_port = 0;
+		int trace_server_sync_trace_port = 0;
+		int trace_server_async_trace_port = 0;
+		TraceConfig();
+	};
+
+	struct TraceEntry
+	{
+		bool     is_track = false;
+		__int64  index = 0;                    ///< log序号
+		string   functrack;                    ///< function follow flag >>	or <<
+		__int64  functime = 0;                 ///< 函数体执行时间
+		int      level = 0;                    ///< log等级
+		string   label;                        ///< log标识	
+		string   threadid;                     ///< 线程id
+		string   processname;                  ///< 进程名
+		string   modulename;                   ///< 模块名
+		string   filename;                     ///< file name;
+		string   funcname;                     ///< function name;
+		int      line = 0;                     ///< line number;
+		__int64  datetime = 0;                 ///< log打印日期时间
+		__int64  runtime = 0;                  ///< log打印时程序运行时间
+		bool     async = false;                ///< 是否为异步log
+		bool     synclock = false;             ///< 同步模式下是否加锁
+		string   content;                      ///< log内容
+		const    string& trace_json_text();
+		const    string& trace_console_text();
+	private:
+		string trace_json_text_;
+		string trace_console_text_;
+	};
+
+
+	class TracePrinter
+	{
+	public:
+		virtual int TraceOutLog(bool is_track, int level, const string& label, const string& module_name, const string& file_name, const string& func_name, int line, const char* log_format, ...) = 0;
+		virtual const TraceConfig& trace_config() const = 0;
+		virtual const TraceConfig& trace_config(const TraceConfig& config) = 0;
+		virtual int WaitTraceThreadEnd() = 0;
+		virtual int CreateKlogConsole() = 0;
+		virtual int GenerateDumpInfo() = 0;
+		virtual int SetKlogConsoleUTF8() = 0;
+
+		static string GetModuleName()
+		{
+			char module_name[100] = { 0 };
+			GetModuleFileNameA(GetSelfModuleHandle(), module_name, 100);
+			return GetFileName(module_name);
+		}
+		static string GetFileName(const string& path_name)
+		{
+			string file_name(path_name);
+			string::size_type pos = file_name.find_last_of("\\/");
+			if (pos != string::npos)
+			{
+				file_name = file_name.substr(pos + 1);
+			}
+			return file_name;
+		}
+	private:
+		static HMODULE GetSelfModuleHandle()
+		{
+			MEMORY_BASIC_INFORMATION mbi;
+			return ((::VirtualQuery(GetSelfModuleHandle, &mbi, sizeof(mbi)) != 0) ? (HMODULE)mbi.AllocationBase : NULL);
+		}
+	};
+
+#ifdef __cplusplus
+	extern "C" {
+#endif
+		KLOGLIB_API TracePrinter* TracePrinterInstance(void);
+#ifdef __cplusplus
+	}
+#endif
+	typedef TracePrinter* (*TracePrinterInstanceFun)(void);
+}
